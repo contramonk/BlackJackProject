a6618cafd824b890c2ffc95a950f99f1a4b37a1d
Jonathan Edwards <edwardjd@colorado.edu>
2016-10-15 00:20:09 -0600

    Save Point

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,32 +1,94 @@
 package game;
 
+import java.util.Scanner;
+
 public class Game {
+	static Deck deck = new Deck(0);
+	static Hand pHand = new Hand();
+	static Hand dHand = new Hand();
+	static Scanner kb = new Scanner(System.in);
+
 	public static void main(String[] args) {
 		run();
-		
+
 	}
+
 	public static void run() {
-		
-		Deck deck = new Deck(0);
-		deck.addCardsToDeck();
-		Hand pHand = new Hand();
-		Hand dHand = new Hand();
-		Player player = new Player("john", pHand, 5000);
-		Dealer dealer = new Dealer("Fred", dHand, 5000);
-		
-		deal(pHand, deck);
-		deal(dHand, deck);
-		deal(pHand, deck);
-		
-		
-		
-		
+
+		introduction();
+		initialDeal();
+		startGame();
+
+		System.out.println(dHand);
 		System.out.println(pHand);
-		
+
 	}
+
 	public static void deal(Hand h, Deck d) {
 		Card deal = d.dealHand();
 		h.addCardToHand(deal);
+
+	}
+
+	public static void startGame() {
+		String hitOrStand = "S";
+		System.out.println("The dealer has " + dHand);
+		System.out.println("You have " + pHand);
+		System.out.println("H) Hit\nS) Stand\nQ) Quit");
 		
+		hitOrStand = kb.next();
+
+		while (!hitOrStand.equals("Q")) {
+			switch (hitOrStand) {
+			case "H":
+				dealDealer();
+				playerTurn();
+				System.out.println("H) Hit\nS) Stand\nQ) Quit");
+				hitOrStand = kb.next();
+				break;
+			case "S":
+				dealDealer();
+				break;
+			}
+		}
+	}
+	public static void playerTurn() {
+		dealPlayer();
+		//if (pHand)
+		
+		
+	}
+
+	public static void dealPlayer() {
+		deal(pHand, deck);
+		System.out.println("Player: " + pHand + "\nHand Total: " + pHand.getHandTotal());
+	}
+
+	public static void dealDealer() {
+		deal(dHand, deck);
+		System.out.println("Dealer: " + dHand + "\nDealer Total: " + dHand.getHandTotal());
+
+	}
+
+	public static void initialDeal() {
+		deck.addCardsToDeck();
+		deal(pHand, deck);
+		deal(dHand, deck);
+		deal(pHand, deck);
+		deal(dHand, deck);
+
+	}
+
+	public static void introduction() {
+
+		System.out.println("Welcome to BlackJack.");
+		System.out.println("What is your name?");
+		String playerName = kb.next();
+		Player player = new Player(playerName, pHand, 5000);
+
+		System.out.println("What would you like your opponents name to be?");
+		String dealerName = kb.next();
+		Dealer dealer = new Dealer(dealerName, dHand, 5000);
+		System.out.println(dealerName + " is dealing.");
 	}
 }
--- src/game/Hand.java
+++ src/game/Hand.java
@@ -3,11 +3,36 @@ package game;
 import java.util.ArrayList;
 
 public class Hand extends ArrayList<Card> {
-
+	private int handTotal = 0;
+	
+	public Hand() {
+		
+	}
+	// Get
+	public int getHandTotal() {
+		return handTotal;
+	}
+	// Set
+	public void setHandTotal(int ht) {
+		this.handTotal = ht;
+	}
 	public void addCardToHand(Card c) {
 
 		this.add(c);
 
+	}
+	
+	public int handValue() {
+		for (int i = 0; i < this.size()/2; i+=2) {
+			try {
+			handTotal += this.get(i).getValue() + this.get(i+1).getValue();
+			}
+			catch(IndexOutOfBoundsException iob) {
+				handTotal += this.get(i).getValue();
+			}
+		}
+		return handTotal;
+		
 	}
 
 }
